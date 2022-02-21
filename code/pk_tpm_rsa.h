404832f230554523a199a3d87fb27d3d6f91d54f
wenxin.leong <wenxin.leong@infineon.com>
2022-02-21 09:55:32 +0800

    edit code
    rename api pk_tpm_rsa.h

--- code/pk_tpm_rsa.h
+++ code/pk_tpm_rsa.h
@@ -10,12 +10,10 @@
 typedef struct
 {
     mbedtls_rsa_context rsa;
-    // handle
 } mbedtls_tpm_rsa;
 
 extern const mbedtls_pk_info_t tpm_rsa_info;
-extern const mbedtls_pk_info_t tpm_ecp_info;
 
-int tpm_pk_init( mbedtls_pk_context *ctx , int padding_scheme, int hash_algo);
+int tpm_rsa_pk_init( mbedtls_pk_context *ctx , int padding_scheme, int hash_algo);
 
 #endif
